7d6f4d041703c43cde6a2740cdcf7309b6a6f539
Serg <gratsli2004@gmail.com>
2023-04-13 21:47:37 +0200

    dodalem flag --init

--- skrypt.sh
+++ skrypt.sh
@@ -12,12 +12,15 @@ elif [[ $1 == "--logs" || $1 == "-l" ]]; then
       echo "log$i.txt created by my_script.sh on $(date)" > log$i.txt
     done
   fi
+elif [[ $1 == "--init" ]]; then
+  git clone https://github.com/SiarheiHratsli/lab4
+  export PATH=$PATH:/path/to/repo
 elif [[ $1 == "--help" || $1 == "-h" ]]; then
   echo "Usage:"
-  echo "  my_script.sh --date                 Display today's date"
-  echo "  my_script.sh --logs [num]           Create [num] log files, default 
+  echo "  my_script.sh --date OR -d                    Display today's date"
+  echo "  my_script.sh --logs [num] OR -l [num]        Create [num] log files, default 
 100"
-  echo "  my_script.sh --help                 Display this help message"
+  echo "  my_script.sh --help OR -h                    Display this help message"
 else
   echo "Invalid option. Use --help to see available options."
 fi
