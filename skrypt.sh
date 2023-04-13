f55f87363b317cf9190e04b28559d7cf1b8bf39b
Serg <gratsli2004@gmail.com>
2023-04-13 21:51:50 +0200

    dodalem flag --error [num]

--- skrypt.sh
+++ skrypt.sh
@@ -2,6 +2,7 @@
 
 if [[ $1 == "--date" || $1 == "-d" ]]; then
   echo $(date)
+
 elif [[ $1 == "--logs" || $1 == "-l" ]]; then
   if [[ -z $2 ]]; then
     for i in {1..100}; do
@@ -12,9 +13,22 @@ elif [[ $1 == "--logs" || $1 == "-l" ]]; then
       echo "log$i.txt created by my_script.sh on $(date)" > log$i.txt
     done
   fi
+
 elif [[ $1 == "--init" ]]; then
   git clone https://github.com/SiarheiHratsli/lab4
   export PATH=$PATH:/path/to/repo
+
+elif [[ $1 == "--error" || $1 == "-e" ]]; then
+  if [[ -z $2 ]]; then
+    for i in {1..100}; do
+      mkdir -p error$i && echo "error $i" > error$i/error$i.txt
+    done
+  else
+    for i in $(seq 1 $2); do
+      mkdir -p error$i && echo "error $i" > error$i/error$i.txt
+    done
+  fi
+
 elif [[ $1 == "--help" || $1 == "-h" ]]; then
   echo "Usage:"
   echo "  my_script.sh --date OR -d                    Display today's date"
