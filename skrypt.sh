6f1fb188c97f4a1cfb38d65c2d68dfa5e56c00c6
Serg <gratsli2004@gmail.com>
2023-04-13 21:45:26 +0200

    dodalem nowy flagi -d -l -h

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ $1 == "--date" ]]; then
+if [[ $1 == "--date" || $1 == "-d" ]]; then
   echo $(date)
-elif [[ $1 == "--logs" ]]; then
+elif [[ $1 == "--logs" || $1 == "-l" ]]; then
   if [[ -z $2 ]]; then
     for i in {1..100}; do
       echo "log$i.txt created by my_script.sh on $(date)" > log$i.txt
@@ -12,13 +12,12 @@ elif [[ $1 == "--logs" ]]; then
       echo "log$i.txt created by my_script.sh on $(date)" > log$i.txt
     done
   fi
-elif [[ $1 == "--help" ]]; then
+elif [[ $1 == "--help" || $1 == "-h" ]]; then
   echo "Usage:"
-  echo "  my_script.sh --date             Display today's date"
-  echo "  my_script.sh --logs [num]      Create [num] log files, default 
+  echo "  my_script.sh --date                 Display today's date"
+  echo "  my_script.sh --logs [num]           Create [num] log files, default 
 100"
-  echo "  my_script.sh --help            Display this help message"
+  echo "  my_script.sh --help                 Display this help message"
 else
   echo "Invalid option. Use --help to see available options."
 fi
-
