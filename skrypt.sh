eb1579e831e3f916194e8c8afb090b92112090f6
Serg <gratsli2004@gmail.com>
2023-04-13 19:55:08 +0200

    dodalem flag --logs

--- skrypt.sh
+++ skrypt.sh
@@ -2,4 +2,14 @@
 
 if [[ $1 == "--date" ]]; then
   echo $(date)
+elif [[ $1 == "--logs" ]]; then
+  if [[ -z $2 ]]; then
+    for i in {1..100}; do
+      echo "log$i.txt created by my_script.sh on $(date)" > log$i.txt
+    done
+  else
+    for i in $(seq 1 $2); do
+      echo "log$i.txt created by my_script.sh on $(date)" > log$i.txt
+    done
+  fi
 fi
